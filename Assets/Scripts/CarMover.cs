f77304c590c186a4f5d1e4ec425e80a366a1fca9
travnik <travnik@inbox.ru>
2020-03-15 20:50:21 +0400

    control tilemap

---
+++ Assets/Scripts/CarMover.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMover : MonoBehaviour
+{
+    public Transform SelfTransform;
+    public float Accelerate = 0.1f;
+    //public float MinSpeed = 0f;
+    public float AngleRotate = 15f;
+
+    private Vector3 _force;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        SelfTransform.position += _force;
+
+        var moveHorizontal = Input.GetAxisRaw("Horizontal");
+        var moveVertical = Input.GetAxisRaw("Vertical");
+        
+        if (moveVertical > 0)
+        {
+            _force += (SelfTransform.up * Time.deltaTime) * Accelerate;
+        }
+        else
+        {
+            _force = Vector3.Lerp(_force, Vector3.zero, Time.deltaTime);
+        }
+
+            SelfTransform.eulerAngles = new Vector3(0, 0, AngleRotate * -moveHorizontal);
+
+    }
+}
